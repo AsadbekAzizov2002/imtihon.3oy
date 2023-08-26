86d341372765228a07ffa9387cebf3663b551362
AsadbekAzizov2002 <azizovasadbek489@gmail.com>
2023-08-26 11:08:57 +0500

    end

---
+++ main.js
@@ -0,0 +1,34 @@
+// let arr =[["i"],["study"],["javaScript"],["right"],["now"]];
+
+// let arry =[]
+
+// arr.splice(3,5);
+
+// console.log(arr);
+
+
+
+
+
+// let str="salom dunyo";
+
+// console.log(str.split.revers.joing(""));
+
+
+
+// let str = "1java2script3 5deve6lo4per";
+// let newArr2=(a)=>{
+//     let num=[],str=[]
+//     for(let i of a){
+//         for(let j of i){
+//             if(tupofJ==Number){
+//                 num.push(i)
+//             }else{
+//                 str.push
+//             }
+//         }
+//     }return
+//         num,
+//         str
+    
+// }
